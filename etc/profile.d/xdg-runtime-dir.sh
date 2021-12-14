94ec8b557a6dee21ce48ffa64e20a8cae4292a10
Leon Schumacher <leonsch@protonmail.com>
2021-12-14 21:42:44 +0100

    Fix permissions for xdg-runtime-dir

--- etc/profile.d/xdg-runtime-dir.sh
+++ etc/profile.d/xdg-runtime-dir.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 XDG_RUNTIME_DIR="/tmp/xdg-runtime-dir-$(id -u)"
 export XDG_RUNTIME_DIR
-mkdir -p "$XDG_RUNTIME_DIR"
+# shellcheck disable=SC2174 # we want -m to only affect the deepest level
+mkdir -m 700 -p "$XDG_RUNTIME_DIR"
